da8f4a05736fd722ccc26544b2759531632cb3c1
Bert Kruiter <b.kruiter.91@gmail.com>
2023-06-19 19:59:48 +0200

    create seperate util file for api related functions

---
+++ src/utils/api.tsx
@@ -0,0 +1,26 @@
+import { Mapping, TradeDataList } from "../models/app-models";
+
+export const transformMappedData = (mappingData: Mapping[]) => {
+  const loadedMapping: Mapping[] = [];
+
+  for (const key in mappingData) {
+    loadedMapping.push({ ...mappingData[key] });
+  }
+
+  return loadedMapping;
+};
+
+export const transformHourPricesData = (hourPricesData: {
+  data: TradeDataList[];
+}) => {
+  const loadedHourPrices: TradeDataList[] = [];
+
+  for (const key in hourPricesData.data) {
+    loadedHourPrices.push({
+      id: Number(key),
+      ...hourPricesData.data[key],
+    });
+  }
+
+  return loadedHourPrices;
+};
