4a09bc28e1245723871ff23179c37d5fed75f87a
Bert Kruiter <b.kruiter.91@gmail.com>
2023-06-25 21:47:56 +0200

    add modal component for future use, restructure UI folder

---
+++ src/UI/modal/Modal.tsx
@@ -0,0 +1,35 @@
+import { Fragment } from "react";
+import ReactDOM from "react-dom";
+
+import classes from "./Modal.module.css";
+
+const Backdrop = (props: { onClick: () => void }) => {
+  return <div className={classes.backdrop} onClick={props.onClick} />;
+};
+
+const ModalOverlay = (props: { children: JSX.Element }) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
+    </div>
+  );
+};
+
+const portalElement = document.getElementById("overlays") as HTMLElement;
+
+const Modal = (props: { onClose: () => void; children: JSX.Element }) => {
+  return (
+    <Fragment>
+      {ReactDOM.createPortal(
+        <Backdrop onClick={props.onClose} />,
+        portalElement
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay>{props.children}</ModalOverlay>,
+        portalElement
+      )}
+    </Fragment>
+  );
+};
+
+export default Modal;
