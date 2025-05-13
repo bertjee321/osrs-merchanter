81b2cdcbbfa5b4aba3d8505f12c2a61f8c3a34cf
bertjee321 <b.kruiter.91@gmail.com>
2025-05-13 22:05:12 +0200

    Refactor App component to streamline data fetching and combine mapping with hour prices

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,42 @@
 import { useEffect, useState } from "react";
-import useHttp from "../src/hooks/use-http";
+import { fetchHourPrices, transformHourlyPriceResponse } from "./api/hour-prices.api";
+import { fetchItemMapping } from "./api/item-mapping.api";
 import { PriceTable } from "./components/PriceTable";
-import { Mapping, PriceDataMapping, TradeDataList } from "./models/app.models";
-import { transformHourPricesData } from "./utils/api";
+import { PriceDataMapping } from "./models/app.models";
 import { combineMappingAndHourPricesList } from "./utils/utils";
 
 function App() {
-  const { sendRequest: fetchMappingData } = useHttp();
-  const { sendRequest: fetchHourPrices } = useHttp();
-
-  const [hourPricesList, setHourPricesList] = useState<TradeDataList[]>([]);
-  const [mappedItems, setMappedItems] = useState<Mapping[]>([]);
   const [fullList, setFullList] = useState<PriceDataMapping[]>([]);
   const [isLoading, setIsLoading] = useState(false);
 
-  const loadData = () => {
-    setIsLoading(true);
-    const getTransformedMapping = (mappingData: Mapping[]) => {
-      setMappedItems(mappingData);
-    };
-    const getTransformedHourPricesData = (hourPricesData: {
-      data: TradeDataList[];
-    }) => {
-      setHourPricesList(transformHourPricesData(hourPricesData));
-    };
-
-    fetchMappingData(
-      {
-        url: "https://prices.runescape.wiki/api/v1/osrs/mapping",
-      },
-      getTransformedMapping
-    );
-    fetchHourPrices(
-      {
-        url: "https://prices.runescape.wiki/api/v1/osrs/1h",
-      },
-      getTransformedHourPricesData
-    );
-  };
-
   useEffect(() => {
-    if (mappedItems.length > 0 && hourPricesList.length > 0) {
-      combineLists();
-    }
-  }, [mappedItems, hourPricesList]);
+    const fetchData = async () => {
+      setIsLoading(true);
+      const mappingData = await fetchItemMapping();
+      const hourPricesData = await fetchHourPrices();
 
-  useEffect(() => {
-    loadData();
+      if (!mappingData || !hourPricesData) {
+        setIsLoading(false);
+        return;
+      }
+
+      const combinedList = combineMappingAndHourPricesList(
+        mappingData,
+        transformHourlyPriceResponse(hourPricesData)
+      );
+
+      setFullList(combinedList);
+      setIsLoading(false);
+    };
+
+    fetchData();
   }, []);
-
-  const combineLists = () => {
-    setFullList(combineMappingAndHourPricesList(mappedItems, hourPricesList));
-    setIsLoading(false);
-  };
 
   return (
     <div className="container text-center">
       <PriceTable data={fullList} loading={isLoading} />
     </div>
+
   );
 }
 
