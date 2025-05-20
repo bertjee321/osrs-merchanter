11c043172d6f55206f32f3ef3bf0a999fe9c26b5
bertjee321 <b.kruiter.91@gmail.com>
2025-05-20 22:15:49 +0200

    Refactor App component: implement routing with BrowserRouter and Layout, and remove redundant export

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { fetchHourPrices } from "./api/hour-prices.api";
 import { fetchItemMapping } from "./api/item-mapping.api";
 import { PriceTable } from "./components/price-table/PriceTable";
 import { PriceDataMapping } from "./models/app.models";
 import { combineMappingAndHourPricesList } from "./utils/utils";
+import { Layout } from "./components/layout/Layout";
 
-export const App = () => {
+const App = () => {
   const [fullList, setFullList] = useState<PriceDataMapping[]>([]);
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
@@ -37,8 +39,33 @@ export const App = () => {
   }, []);
 
   return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Navigate to="/items" replace />} />
+          <Route
+            path="items"
+            element={
+              <PriceTable data={fullList} loading={isLoading} error={isError} />
+            }
+          />
+          {/* <Route path="items/:itemId" element={<ItemDetail />} /> */}
+          <Route
+            path="*"
+            element={
+              <div className="text-center mt-5">404 - Page not found</div>
+            }
+          />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+
+  return (
     <div className="container text-center">
       <PriceTable data={fullList} loading={isLoading} error={isError} />
     </div>
   );
-}
+};
+
+export default App;
