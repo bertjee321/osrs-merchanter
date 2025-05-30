f71648634c926397103b40543827f2426649be1d
bertjee321 <b.kruiter.91@gmail.com>
2025-05-30 23:06:29 +0200

    Refactor AlchInfo component to enhance display of alchemy information and improve formatting

--- src/components/item-details/alch-info/AlchInfo.tsx
+++ src/components/item-details/alch-info/AlchInfo.tsx
@@ -1,20 +1,67 @@
-// Show:
-// - High-alch price
-// - Cost price nature rune
-// - Cost price fire rune (optional)
-// - Profit/loss per alch and total
+import { Card, OverlayTrigger, Tooltip } from "react-bootstrap";
+import { PriceDataMapping } from "../../../models/app.models";
+import { useFullItemsById } from "../../../contexts/FullItemDetailsContext";
 
-export const AlchInfo = () => {
+interface AlchInfoProps {
+  itemDetails: PriceDataMapping;
+}
+
+export const AlchInfo = ({ itemDetails }: AlchInfoProps) => {
+  const natureRuneInfo = useFullItemsById("561");
+
+  const avgNatureRunePrice = (() => {
+    if (!natureRuneInfo) return NaN;
+
+    return Math.floor(
+      (natureRuneInfo?.avgLowPrice + natureRuneInfo?.avgHighPrice) / 2
+    );
+  })();
+
+  // Dummy data
+  const avgMarketPrice = Math.floor(
+    (itemDetails?.avgLowPrice + itemDetails?.avgHighPrice) / 2
+  );
+  const cost = avgMarketPrice + avgNatureRunePrice;
+  const profit = itemDetails?.highalch - cost;
+
+  const formatGp = (value: number) => `${value?.toLocaleString()} gp`;
+
   return (
-    <div className="">
-      <div className="card-body">
-        <h5 className="card-title">Alch Info</h5>
-        <p className="card-text">High-alch price: 1000</p>
-        <p className="card-text">Cost price nature rune: 100</p>
-        <p className="card-text">Cost price fire rune: 50</p>
-        <p className="card-text">Profit/loss per alch: 850</p>
-        <p className="card-text">Total profit/loss: 8500</p>
+    <Card.Body>
+      <Card.Title>🧪 Alch Info - {itemDetails?.name}</Card.Title>
+
+      <div className="mb-2">
+        <strong>📈 High Alch:</strong> {formatGp(itemDetails?.highalch)}
       </div>
-    </div>
+
+      <div className="mb-2">
+        <strong>🛒 Item avg. price (1h):</strong> {formatGp(avgMarketPrice)}
+      </div>
+
+      <div className="mb-2">
+        <strong>
+          <OverlayTrigger
+            placement="top"
+            overlay={
+              <Tooltip>
+                Average price of nature runes based on the last hour (high + low
+                / 2).
+              </Tooltip>
+            }
+          >
+            <span style={{ cursor: "help" }}>🔋 Nature Rune Cost:</span>
+          </OverlayTrigger>
+        </strong>{" "}
+        {formatGp(avgNatureRunePrice)}
+      </div>
+
+      <hr />
+
+      <div
+        className={`fw-bold ${profit >= 0 ? "text-success" : "text-danger"}`}
+      >
+        💰 Verwachte winst per alch: {formatGp(profit)}
+      </div>
+    </Card.Body>
   );
 };
