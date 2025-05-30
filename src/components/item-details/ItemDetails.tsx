c781003eead873d898a935dd5d8043abe27a1e63
bertjee321 <b.kruiter.91@gmail.com>
2025-05-30 23:06:32 +0200

    Refactor ItemDetails component to improve data fetching, error handling, and display of item details

--- src/components/item-details/ItemDetails.tsx
+++ src/components/item-details/ItemDetails.tsx
@@ -1,27 +1,42 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import { fetchItemLatestPrice } from "../../api/get-item.api";
-import { useItemMapping } from "../../contexts/ItemMappingContext";
+import { useFullItemsById } from "../../contexts/FullItemDetailsContext";
+import { ItemPriceData } from "../../models/app.models";
 import { BackButton } from "../../UI/buttons/BackButton";
 import { AlchInfo } from "./alch-info/AlchInfo";
 import { ItemActions } from "./item-actions/ItemActions";
-import { PriceGraph } from "./price-graph/PriceGraph";
 import { PricesOverview } from "./prices-overview/PricesOverview";
 import { ProfitCalculator } from "./profit-calculator/ProfitCalculator";
-import { SetConversionInfo } from "./set-conversion-info/SetConversionInfo";
-import { TimeSelector } from "./time-selector/TimeSelector";
+import { LoadingGrid } from "../../UI/loading-grid/LoadingGrid";
 
 export const ItemDetails = () => {
-  console.log("ItemDetails component rendered");
   const { itemId } = useParams();
-  const itemMapping = useItemMapping();
-  const item = itemMapping.find((item) => item.id === Number(itemId));
+  const itemDetails = useFullItemsById(itemId!);
+  const [isLoading, setIsLoading] = React.useState(true);
+  const [hasError, setHasError] = React.useState<boolean>(false);
+  const [latestTradeData, setLatestTradeData] = React.useState<ItemPriceData>({
+    high: 0,
+    highTime: 0,
+    low: 0,
+    lowTime: 0,
+  });
 
   React.useEffect(() => {
     const fetchData = async () => {
+      setIsLoading(true);
+      setHasError(false);
+
       const tradeData = await fetchItemLatestPrice(itemId!);
 
-      console.log("Trade data:", tradeData);
+      if (!tradeData) {
+        setHasError(true);
+        setIsLoading(false);
+        return;
+      }
+
+      setLatestTradeData(tradeData);
+      setIsLoading(false);
     };
 
     fetchData();
@@ -34,45 +49,78 @@ export const ItemDetails = () => {
         <h2 className="mb-0">
           <img
             src={`https://static.runelite.net/cache/item/icon/${itemId}.png`}
-            alt={item?.name}
+            alt={itemDetails?.name}
           />
-          {item?.name}
+          {itemDetails?.name}
         </h2>
       </div>
 
       <ItemActions itemId={itemId!} />
-      <PricesOverview />
 
-      <div className="card mb-4">
+      {isLoading && (
+        <div className="text-center">
+          <LoadingGrid />
+          <h4>Fetching data ...</h4>
+        </div>
+      )}
+
+      {hasError && (
+        <div className="alert alert-danger" role="alert">
+          Failed to fetch item details. Please try again later.
+        </div>
+      )}
+
+      {!itemDetails && !isLoading && (
+        <div className="alert alert-warning" role="alert">
+          Item details not found.
+        </div>
+      )}
+
+      {!isLoading && !hasError && itemDetails && (
+        <>
+        <PricesOverview data={{ latestTradeData, itemDetails: itemDetails! }} />
+
+      {/* <div className="card mb-4">
         <div className="card-body">
           <h5 className="card-title">Price History</h5>
           <TimeSelector itemId={itemId!} />
           <PriceGraph />
         </div>
-      </div>
+      </div> */}
 
       <div className="row align-items-stretch">
         <div className="col-md-6 mb-4 d-flex">
-          <div className="card h-100 w-100">
-            <div className="card-body">
-              <ProfitCalculator />
-            </div>
+          <div className="card h-100 w-100 mb-4 shadow-sm">
+            <ProfitCalculator
+              prices={{
+                average: {
+                  buy: itemDetails?.avgLowPrice || 0,
+                  sell: itemDetails?.avgHighPrice || 0,
+                },
+                snapshot: {
+                  buy: latestTradeData.low,
+                  sell: latestTradeData.high,
+                },
+              }}
+            />
           </div>
         </div>
         <div className="col-md-6 mb-4 d-flex">
-          <div className="card h-100 w-100">
-            <div className="card-body">
-              <AlchInfo />
-            </div>
+          <div className="card h-100 w-100 mb-4 shadow-sm">
+            <AlchInfo itemDetails={itemDetails!} />
           </div>
         </div>
       </div>
 
-      <div className="card mb-4">
+      {/* <div className="card mb-4">
         <div className="card-body">
           <SetConversionInfo />
         </div>
-      </div>
+      </div> */}
+      </>
+      )}
+    
+      
     </div>
   );
 };
